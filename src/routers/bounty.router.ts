1259cd6aae6b4c13730fa90db409291e8763cf96
SolForte <mateusbrandao1234@gmail.com>
2023-07-01 05:49:03 -0300

    feat: crud bounty routes

--- src/routers/bounty.router.ts
+++ src/routers/bounty.router.ts
@@ -1,7 +1,29 @@
 import { Router } from "express";
+import bountyController from "../controllers/bounty.controller";
+import { validateSchemaMiddleware } from "../middleware/schemaValidation.middleware";
+import {
+  createBountySchema,
+  deleteBountySchema,
+  updateBountySchema,
+} from "../schema/bountySchema";
 
 const bountyRouter = Router();
 
-// ROTAS
+bountyRouter.post(
+  "/bounty",
+  validateSchemaMiddleware(createBountySchema),
+  bountyController.createPirateBounty
+);
+bountyRouter.get("/bounty", bountyController.getAllPirateBounties);
+bountyRouter.put(
+  "/bounty",
+  validateSchemaMiddleware(updateBountySchema),
+  bountyController.updatePirateBountyByName
+);
+bountyRouter.delete(
+  "/bounty",
+  validateSchemaMiddleware(deleteBountySchema),
+  bountyController.deletePirateBountyByName
+);
 
 export default bountyRouter;
