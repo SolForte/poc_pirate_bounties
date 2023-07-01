454d0f77164138dc59119afce3396171fc59bdf7
SolForte <mateusbrandao1234@gmail.com>
2023-07-01 04:50:23 -0300

    feat: app and router (empty)

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import bountyRouter from "./routes/bounty.router";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(bountyRouter);
+
+const port: number = +process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
+})
---
+++ src/routes/bounty.router.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const bountyRouter = Router();
+
+// ROTAS
+
+export default bountyRouter;
